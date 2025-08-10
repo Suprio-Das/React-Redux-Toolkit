5bbe642d8f31b19f171626ee38fb1ff3141b903d
Suprio-Das <priyamd507@gmail.com>
2025-08-10 23:47:01 +0600

    reset action created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { decrement, increment } from './Redux/CounterSlice';
+import { decrement, increment, reset } from './Redux/CounterSlice';
 
 function App() {
   const count = useSelector((state) => state.counter.value);
@@ -12,11 +12,15 @@ function App() {
   function handleDecrement() {
     dispatch(decrement());
   }
+  function handleReset() {
+    dispatch(reset());
+  }
   return (
     <div>
       <button onClick={handleDecrement}>-</button>
       <p>Count: {count}</p>
       <button onClick={handleIncrement}>+</button>
+      <button onClick={handleReset}>Reset</button>
     </div>
   )
 }
